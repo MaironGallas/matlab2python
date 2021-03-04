1184f692bf30d492adcf4ec881e1f0b408fd7a2c
mairon-lapes <mairongallas@gmail.com>
2021-03-04 17:51:44 -0300

    Criada função para localizador

--- test_trustregion.py
+++ test_trustregion.py
@@ -5,11 +5,18 @@ import numpy as np
 def func(x, a, b, c):
     return a * np.exp(-b * x) + c
 
+def func2(xdata, a, b, w):
+    return a*np.sin(np.pi*(xdata-b/w))
+
+
 if __name__ == '__main__':
 
-    xdata = np.linspace(0, 4, 50)
-    y = func(xdata, 2.5, 1.3, 0.5)
-    np.random.seed(1729)
+    w = 8333.46406
+
+    xdata = np.array([-5612.84560510564, 5625.24223537838, -5644.87683290912, 5675.34436231571, -5691.41421664163])
+    y = np.array([1, 8331, 16659, 24984, 33313])
+
+    """np.random.seed(1729)
     y_noise = 0.2 * np.random.normal(size=xdata.size)
     ydata = y + y_noise
     plt.plot(xdata, ydata, 'b-', label='data')
@@ -19,4 +26,4 @@ if __name__ == '__main__':
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend()
-    plt.show()
+    plt.show()"""
